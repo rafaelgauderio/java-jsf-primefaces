c8276a01e0514c07bef728805d8a693ca1ae4d7d
Rafael de Luca <deluca1712@gmail.com>
2022-01-10 15:58:46 -0300

    SelectOneMenu dinamico

--- Java Resources/br/com/delucahigiene/controller/AtividadeMB.java
+++ Java Resources/br/com/delucahigiene/controller/AtividadeMB.java
@@ -1,5 +1,46 @@
 package br.com.delucahigiene.controller;
 
-public class AtividadeMB {
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import br.com.delucahigiene.model.Atividade;
+import br.com.delucahigiene.model.TipoAtividade;
+
+@Named("atividadeMB")
+@SessionScoped
+public class AtividadeMB  implements Serializable{
+
+
+	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private Atividade atividade;
+	
+	public Atividade getAtividade() {
+		return atividade;
+	}
+
+	public void setAtividade(Atividade atividade) {
+		this.atividade = atividade;
+	}
+	
+	
+	public TipoAtividade[] getAtividades() {
+		return TipoAtividade.values();
+	}	
+	
+
+
+
+
+
+	
+	
+	
+	
+	
 
 }
--- Java Resources/br/com/delucahigiene/model/Atividade.java
+++ Java Resources/br/com/delucahigiene/model/Atividade.java
@@ -10,6 +10,26 @@ public class Atividade implements Serializable{
 	private Long id;
 	private String nome;
 	private TipoAtividade tipoAtividade;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public TipoAtividade getTipoAtividade() {
+		return tipoAtividade;
+	}
+	public void setTipoAtividade(TipoAtividade tipoAtividade) {
+		this.tipoAtividade = tipoAtividade;
+	}
+	
+	
 	
 	
 
